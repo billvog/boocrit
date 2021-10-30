c35c2e46ca374841f9aa0940acdb94fb0f7fc03e
billvog <billvog04@gmail.com>
2021-10-30 21:08:26 +0300

    Added a validation schema on login

--- packages/controller/src/index.ts
+++ packages/controller/src/index.ts
@@ -4,6 +4,8 @@ export * from "./MyApolloClient";
 
 // Login Controller
 export * from "./modules/user/Login";
+export * from './modules/user/Login/ValidationSchema'
+export * from './modules/user/Validation/index'
 
 // Error map
 export * from "./types/ErrorMap";
--- packages/web/modules/Login/LoginView.tsx
+++ packages/web/modules/Login/LoginView.tsx
@@ -1,10 +1,15 @@
-import { ErrorMap, LoginFormValues } from "@boocrit/controller";
+import {
+  ErrorMap,
+  LoginFormValues,
+  LoginValidationSchema,
+} from "@boocrit/controller";
 import { Form, FormikProps, withFormik } from "formik";
 import Link from "next/link";
 import React from "react";
 import { Header } from "../ui/Header";
 import { InputField } from "../ui/InputField";
 import { MyButton } from "../ui/MyButton";
+import { MyError } from "../ui/MyError";
 
 interface LoginViewProps {
   submit: (values: LoginFormValues) => Promise<ErrorMap | null>;
@@ -22,16 +27,11 @@ export const C: React.FC<LoginViewProps & FormikProps<LoginFormValues>> = ({
           className="py-24 space-y-4 flex flex-col items-end"
           style={{ width: 300 }}
         >
-          {(errors as any)._ && (
-            <div className="font-slab font-medium text-red-500 text-left w-full">
-              {(errors as any)._}
-            </div>
-          )}
-          <InputField
-            name="email"
-            placeholder="Email or Username"
-            type="text"
-          />
+          <h2 className="font-slab w-full text-center text-2xl font-bold text-secondary pb-4">
+            Sign in to your account
+          </h2>
+          {(errors as any)._ && <MyError>{(errors as any)._}</MyError>}
+          <InputField name="email" placeholder="Email" type="text" />
           <InputField name="password" placeholder="Password" type="password" />
           <MyButton isLoading={isSubmitting} type="submit">
             <span className="font-slab">Login</span>
@@ -49,6 +49,7 @@ export const C: React.FC<LoginViewProps & FormikProps<LoginFormValues>> = ({
 };
 
 export const LoginView = withFormik<LoginViewProps, LoginFormValues>({
+  validationSchema: LoginValidationSchema,
   mapPropsToValues: () => ({
     email: "",
     password: "",
